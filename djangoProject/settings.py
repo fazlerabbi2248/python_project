4caacbba81fae9f5de112b7093bfdb3b5d75ebbd
unknown <fazlerabbi2248@gmail.com>
2022-06-07 19:27:52 +0600

    user_registration

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
