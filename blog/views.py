0d03d2016263a173a9931113ae20ed02b2351ba0
unknown <fazlerabbi2248@gmail.com>
2022-06-09 01:58:14 +0600

    create model for blog post and connect

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import PostModel
 
 def home(request):
-    return render(request,'blog/home.html')
+    posts = PostModel.objects.all()
+    return render(request,'blog/home.html',{'posts': posts})
 
 def about(request):
     return render(request,'blog/about.html')
